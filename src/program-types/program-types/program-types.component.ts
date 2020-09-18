2e39ceb14bc02eeabf4b24c35708a0c236c329e8
Nicoleta-PredaCarp <nicoleta.predacarp@gmail.com>
2020-09-18 16:24:14 +0300

    establish connection with API for program-types.component.ts

--- src/program-types/program-types/program-types.component.ts
+++ src/program-types/program-types/program-types.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {ProgramType} from "../../models/programTypeModel/program-type";
+import {ProgramTypeService} from "../../services/program-type-service/program-type.service";
 
 @Component({
   selector: "app-program-types",
@@ -7,17 +8,16 @@ import {ProgramType} from "../../models/programTypeModel/program-type";
   styleUrls: ["./program-types.component.css"]
 })
 export class ProgramTypesComponent implements OnInit {
+  public programTypes: ProgramType[] = []
 
-  public programTypes: ProgramType[] = [
-    {id: 1, name: "normal schedule", startsAt: "08:00:00", endsAt: "16:00:00"},
-    {id: 2, name: "first shift", startsAt: "06:00:00", endsAt: "14:00:00"},
-    {id: 3, name: "second shift", startsAt: "14:00:00", endsAt: "22:00:00"},
-    {id: 4, name: "third shift", startsAt: "22:00:00", endsAt: "06:00:00"},
-  ];
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private service: ProgramTypeService,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.service.getAll().subscribe(value => this.programTypes = value);
+  }
 
 }
