6b90b7e748ea38f96964de30391eaf88b8599878
Nicoleta-PredaCarp <nicoleta.predacarp@gmail.com>
2020-09-30 19:59:44 +0300

    confirmation edit to department-edit

--- src/components/departments/department-edit/department-edit.component.ts
+++ src/components/departments/department-edit/department-edit.component.ts
@@ -5,6 +5,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {OvertimeHour} from "../../../models/overTimeModel/overtime-hour";
 import {DepartmentProgram} from "../../../models/departmentProgramModel/department-program";
 import {DepartmentProgramService} from "../../../services/department-program-service/department-program.service";
+import {ConfirmationService} from "primeng/api";
 
 @Component({
   selector: "app-department-edit",
@@ -20,8 +21,16 @@ export class DepartmentEditComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private service: DepartmentsService,
+    private confirmationService: ConfirmationService,
     private depProgramService: DepartmentProgramService,
   ) {
+  }
+
+  ngOnInit(): void {
+    this.getModel();
+  }
+
+  private getModel(): void {
     this.route.paramMap
       .subscribe(params => {
         const id = parseInt(params.get("id"), 10);
@@ -29,14 +38,21 @@ export class DepartmentEditComponent implements OnInit {
       });
   }
 
-  ngOnInit(): void {
-  }
-
-  save(): void {
+  saveVoid(): void {
     this.service.save(this.department).subscribe(value => {
       this.department = value;
       this.gotoList();
     });
+  }
+
+  saveBoolean(): boolean {
+    this.confirmationService.confirm({
+      message: "Are you sure you want to save this item?",
+      accept: () => {
+        this.saveVoid();
+      }
+    });
+    return false;
   }
 
   cancel(): void {
